52c40788efc364f17c374aca1beadd824fbf104b
Leonardo Fragozo <lqfragozo@gmail.com>
2022-01-29 12:16:37 -0300

    rails new with models

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :login, type: String
+  field :password, type: String
+
+  validates :login, presence: true
+  validates :login, uniqueness: true
+
+  validates :password, length: { minimum: 8 }
+  validates :password, presence: true, on: :create
+end
